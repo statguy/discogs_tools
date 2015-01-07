62363108792b2ca4b972ff60fe67a238c7a69cf6
statguy <jvj@iki.fi>
2015-01-07 19:24:38 +0700

    Retry request when failed

--- export_wantlist_marketplace_search_results.py
+++ export_wantlist_marketplace_search_results.py
@@ -4,12 +4,15 @@ import argparse
 import csv
 import urllib.request
 import json
+import time
 
 parser = argparse.ArgumentParser(description='exports wantlist search results from Discogs Marketplace')
 parser.add_argument('wantlist', help='exported CSV wantlist file from Discogs')
 parser.add_argument('output', help='output CSV file')
 args = parser.parse_args()
 
+max_tries = 20
+try_delay = 1
 write_header = True
 
 with open(args.wantlist) as wantlist_file:
@@ -24,17 +27,28 @@ with open(args.wantlist) as wantlist_file:
             title = record[3]
             release_id = record[7]
             url = "http://api.discogs.com/marketplace/search?release_id=" + release_id
-            print("Processing " + artist + " - " + title + "...")
+            print("Processing " + artist + " - " + title + "...") # There is some encoding problem here in Windows
 
-            with urllib.request.urlopen(url) as data:
-                result = data.read().decode()
-                items = json.loads(result)
-                
-                if write_header:
-                    write_header = False
-                    header = [list(items[0].keys())]
-                    output_writer.writerows(header)
-                    
-                for item in items:
-                    row = [list(item.values())]
-                    output_writer.writerows(row)
+            tries = 0
+            while max_tries > tries:
+                try:
+                    with urllib.request.urlopen(url) as data:
+                        result = data.read().decode()
+                        items = json.loads(result)
+                        
+                        if write_header:
+                            write_header = False
+                            header = [list(items[0].keys())]
+                            output_writer.writerows(header)
+                            
+                        for item in items:
+                            row = [list(item.values())]
+                            output_writer.writerows(row)
+                                
+                except:
+                    print("Request failed. Retrying...")
+                    tries += 1
+                    time.sleep(try_delay)
+                    continue
+                else:
+                    break
